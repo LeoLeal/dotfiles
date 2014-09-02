decc30f437556244e79b8f7b3d5b0d90b5622d9e
Leonardo Leal <dev@leoleal.com.br>
2014-09-02 16:17:58 -0300

    Update softwares.sh

--- files/.bash/bin/softwares.sh
+++ files/.bash/bin/softwares.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
 # homebrew-cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
@@ -15,20 +18,12 @@ brew cask install filezilla
 brew cask install sourcetree
 brew cask install netbeans
 brew cask install vagrant
-brew cask install vmware-fusion
 
 # photography
 brew cask install adobe-creative-cloud
 
 # other
 brew cask install flash-player
-brew cask install alfred
-brew cask install screenflow
-brew cask install dropbox
 brew cask install google-drive
-brew cask install mou
 brew cask install skype
-brew cask install spotify
-brew cask install transmission
 brew cask install tvshows
-brew cask install trim-enabler

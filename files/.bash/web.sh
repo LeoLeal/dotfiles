db0454868585ba7455f0d41d05a195a141ff627f
Leonardo Leal <dev@leoleal.com.br>
2014-09-02 16:02:46 -0300

    Delete web.sh

--- files/.bash/web.sh
+++
@@ -1,5 +0,0 @@
-# Screw you, "no nodename or servname not provided". I'll do `whois
-# http://google.com/` if I want.
-function whois() {
-  command whois $(echo $1 | sed -e 's|https?://||' -e 's|/||g')
-}

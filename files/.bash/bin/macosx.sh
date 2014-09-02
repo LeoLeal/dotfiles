3a98caf0b40f2fa4d81753b03415c5a56b3f784b
Leonardo Leal <dev@leoleal.com.br>
2014-09-02 15:50:37 -0300

    Update macosx.sh

--- files/.bash/bin/macosx.sh
+++ files/.bash/bin/macosx.sh
@@ -3,14 +3,14 @@
 #################################################################
 
 # Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+## sudo nvram SystemAudioVolume=" "
 
 # Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+##defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # Show remaining battery time; hide percentage
-defaults write com.apple.menuextra.battery ShowPercent -string "NO"
-defaults write com.apple.menuextra.battery ShowTime -string "YES"
+##defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+##defaults write com.apple.menuextra.battery ShowTime -string "YES"
 
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -21,7 +21,7 @@ defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
 # Hide the Spotlight Menu Icon
-sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+##sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
@@ -37,7 +37,7 @@ defaults write com.apple.dashboard mcx-disabled -boolean NO
 #################################################################
 
 # Disable guest user
-defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool NO
+##defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool NO
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
@@ -48,13 +48,13 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 #################################################################
 
 # Add dock on the left
-defaults write com.apple.dock orientation -string left
+##defaults write com.apple.dock orientation -string left
 
 # Add dock on the center
-defaults write com.apple.dock pinning -string middle;
+##defaults write com.apple.dock pinning -string middle;
 
 # Disable hide and show the Dock
-defaults write com.apple.dock autohide -bool false
+##defaults write com.apple.dock autohide -bool false
 
 # Enable spring loading for all Dock items
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
@@ -63,10 +63,10 @@ defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 defaults write com.apple.dock show-process-indicators -bool true
 
 # Enable the 2D Dock
-defaults write com.apple.dock no-glass -bool true
+##defaults write com.apple.dock no-glass -bool true
 
 # Set sidebar icon size to medium
-defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+##defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
 
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
@@ -91,9 +91,9 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
 # Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+##defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+##defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+##defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -105,7 +105,7 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
 # Remove the spring loading delay for directories
-defaults write NSGlobalDomain com.apple.springing.delay -float 0
+defaults write NSGlobalDomain com.apple.springing.delay -float 0.5
 
 # Always show the user library folder
 chflags nohidden ~/Library/
@@ -115,7 +115,7 @@ chflags nohidden ~/Library/
 #################################################################
 
 # Empty Trash securely by default
-defaults write com.apple.finder EmptyTrashSecurely -bool true
+##defaults write com.apple.finder EmptyTrashSecurely -bool true
 
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
@@ -125,9 +125,9 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 #################################################################
 
 # Enable tap to click (Trackpad) for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+##defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+##defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+##defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Trackpad: map bottom right corner to right-click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
@@ -139,29 +139,29 @@ defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryCli
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Increase sound quality for Bluetooth headphones/headsets
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+##defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 # Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+##defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+##defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 #################################################################
 # Safari & WebKit
 #################################################################
  
 # Set Safari’s home page to `about:blank` for faster loading
-defaults write com.apple.Safari HomePage -string "about:blank"
+##defaults write com.apple.Safari HomePage -string "about:blank"
 
 # Hide Safari’s bookmarks bar by default
-defaults write com.apple.Safari ShowFavoritesBar -bool false
+##defaults write com.apple.Safari ShowFavoritesBar -bool false
 
 # Hide Safari’s sidebar in Top Sites
-defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+##defaults write com.apple.Safari ShowSidebarInTopSites -bool false
 
 # Disable Safari’s thumbnail cache for History and Top Sites
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+##defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
 # Tell Safari to open new window links in tabs
 defaults write com.apple.Safari TargetedClicksCreateTabs -bool true
@@ -170,10 +170,10 @@ defaults write com.apple.Safari TargetedClicksCreateTabs -bool true
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
 # Make Safari’s search banners default to Contains instead of Starts With
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+##defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
 # Remove useless icons from Safari’s bookmarks bar
-defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+##defaults write com.apple.Safari ProxiesInBookmarksBar "()"
 
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -188,10 +188,10 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 #################################################################
 
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+##defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
-defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9"
+##defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9"
 
 #################################################################
 # Terminal
@@ -222,13 +222,13 @@ defaults write com.apple.ActivityMonitor SortDirection -int 0
 #################################################################
 
 # Enable the debug menu in Address Book
-defaults write com.apple.addressbook ABShowDebugMenu -bool true
+##defaults write com.apple.addressbook ABShowDebugMenu -bool true
 
 # Enable Dashboard dev mode (allows keeping widgets on the desktop)
-defaults write com.apple.dashboard devmode -bool true
+##defaults write com.apple.dashboard devmode -bool true
 
 # Enable the debug menu in iCal (pre-10.8)
-defaults write com.apple.iCal IncludeDebugMenu -bool true
+##defaults write com.apple.iCal IncludeDebugMenu -bool true
 
 # Use plain text mode for new TextEdit documents
 defaults write com.apple.TextEdit RichText -int 0
@@ -237,15 +237,15 @@ defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
 # Enable the debug menu in Disk Utility
-defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
-defaults write com.apple.DiskUtility advanced-image-options -bool true
+##defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+##defaults write com.apple.DiskUtility advanced-image-options -bool true
 
 #################################################################
 # Screen
 #################################################################
 
 # Enable subpixel font rendering on non-Apple LCDs
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+##defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true

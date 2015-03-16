acd25384233cb147ace36dc3cc03a3df16ac93b7
Leonardo Leal <dev@leoleal.com.br>
2015-03-16 12:11:54 -0300

    Delete vagrant.sh

--- files/.bash/vagrant.sh
+++
@@ -1,7 +0,0 @@
-alias vs="vagrant ssh"
-alias vu="vagrant up"
-alias vd="vagrant halt"
-alias vp="vagrant provision"
-
-export VAGRANT_VMWARE_FUSION_APP="/Applications/VMware Fusion.app"
-export VAGRANT_DEFAULT_PROVIDER="vmware_fusion"

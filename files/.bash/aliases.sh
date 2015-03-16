14a837dcde7d08231e7cca2ef484ce216cd4ab58
Leonardo Leal <dev@leoleal.com.br>
2015-03-16 12:15:41 -0300

    Update aliases.sh

--- files/.bash/aliases.sh
+++ files/.bash/aliases.sh
@@ -1,21 +1,3 @@
-# Shortcuts
-alias ..="cd .."
-alias ll="ls -Glahs"
-alias ls="ls -Gh"
-alias cl="clear"
-alias psgrep="ps aux | grep -v grep | grep"
-alias reload="source ~/.bash_profile"
-alias subl="subl -n"
-
-# PS
-alias psa="ps aux"
-alias psg="ps aux | grep "
-alias psr="ps aux | grep ruby"
-alias psj="ps aux | grep java"
-
-# Edit hosts file
-alias hosts="sudo vim /etc/hosts"
-
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
@@ -26,28 +8,7 @@ alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 # Recursively delete `.DS_Store` files
-alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
-
-# Empty the Trash on all mounted volumes and the main HDD
-# Also, clear Apple’s System Logs to improve shell startup speed
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+alias cleandsstore="find . -type f -name '*.DS_Store' -ls -delete"
 
 # Copy public key to pasteboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# Enable/ Disable Spotlight
-alias spoton="sudo mdutil -a -i on"
-alias spotoff="sudo mdutil -a -i off"
-
-# Start a simple http server on port 8000
-alias server="python -m SimpleHTTPServer 8000"
-
-# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update="sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update"
-
-# Update dotfiles
-alias redot="curl -Ls https://raw.github.com/LeoLeal/dotfiles/master/install.bash | bash"
